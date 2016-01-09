dc82b1732e8b33ff2bf0131efeb49d5cceb35c00
John Hewitt <john.hewitt64@gmail.com>
2016-01-09 18:25:54 -0500

    fixed bitext augment setup

--- setup_bitext_augments.sh
+++ setup_bitext_augments.sh
@@ -8,22 +8,23 @@
 
 # First argument: the normalization constant (or the quota, whichever you prefer.)
 norm_constant=$1
+augment_dir=$2
 
 # Second argument: index for the experiment...
 
 # For each augmentation model in the symlinked augmodel directory,
-for aug_model in $(ls augment_models); do
-    augment_lang=${aug_model: -6:3}
+for aug_model in $(ls $augment_dir/aug* ); do
+    augment_lang=${aug_model: -3:3}
     for lang in $(ls data); do
         echo $augment_lang $lang
         # Find the data folder of the language corresponding to the model.
         if [ "$lang" = "$augment_lang" ]; then
             # For each source already present,
-            for src in $(ls data/$lang); do
+            for src in $(ls data/$lang ); do
                 echo $augment_lang $lang $src
                 # Construct an augmented source that combines the bitext and the phrase table
                 # int a new bitext.
-                ~/joshua-scripts/weight_tables.py $PWD/data/$lang/$src/trn.eng $PWD/data/$lang/$src/trn.$lang augment_models/$aug_model $norm_constant
+                ~/joshua-scripts/weight_tables.py $PWD/data/$lang/$src/trn.eng $PWD/data/$lang/$src/trn.$lang $aug_model $norm_constant
                 # This new bitext is currently sitting in the directory in which thie script
                 # is run, so put it in its own data folder. Then, when the script creation
                 # file is run, it *should* create qsub scripts for the original as well as
@@ -32,8 +33,8 @@ for aug_model in $(ls augment_models); do
                 # make an interesting comparison. (They are likely done elsewhere.)
                 new_folder=$PWD/data/$lang/${src}_aug/
                 mkdir $new_folder
-                mv ${norm_constant}x_combined_.$src.$lang $new_folder/trn.$lang
-                mv ${norm_constant}x_combined_.$src.eng $new_folder/trn.eng
+                mv ${norm_constant}x_combined_.trn.$lang $new_folder/trn.$lang
+                mv ${norm_constant}x_combined_.trn.eng $new_folder/trn.eng
 
                 # Now also copy the same dev data
                 cp $PWD/data/$lang/$src/dev.eng $new_folder/dev.eng
